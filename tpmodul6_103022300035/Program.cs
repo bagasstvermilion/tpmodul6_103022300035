86ae8e51bd9959bdcba5033b9775c8aa383f13c0
Bagas Pratama <bagassss.com@gmail.com>
2025-03-24 00:11:09 +0700

    menambahkan class SayaTubeVideo

--- tpmodul6_103022300035/Program.cs
+++ tpmodul6_103022300035/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using tpmodul6_103022300035;
+
+class Program
+{
+    static void Main()
+    {
+        SayaTubeVideo video = new SayaTubeVideo("Tutorial Design By Contract - Bagas Pratama");
+
+        video.PrintVideoDetails();
+        video.IncreasePlayCount(100);
+        video.PrintVideoDetails();
+    }
+}
---
+++ tpmodul6_103022300035/SayaTubeVideo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul6_103022300035
+{
+    class SayaTubeVideo
+    {
+        private int id;
+        private string title;
+        private int playCount;
+
+        public SayaTubeVideo(string title)
+        {
+            Random rand = new Random();
+            this.id = rand.Next(10000, 99999);
+            this.title = title;
+            this.playCount = 0;
+        }
+
+        public void IncreasePlayCount(int increment)
+        {
+            this.playCount += increment;
+        }
+
+        public void PrintVideoDetails()
+        {
+            Console.WriteLine($"ID Video: {id}");
+            Console.WriteLine($"Judul: {title}");
+            Console.WriteLine($"Play Count: {playCount}");
+        }
+    }
+
+}
